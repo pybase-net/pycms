4610021d835c2ccc11326ec04695496edc943239
misostack <misostack.com@gmail.com>
2025-02-20 17:09:19 +0700

    Add sample SPA

--- apps/backoffice/views.py
+++ apps/backoffice/views.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'backoffice/index.html')
+    return render(request, 'backoffice/index.html', {
+        'debug': settings.DEBUG
+    })
--- cms/urls.py
+++ cms/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('backoffice/', include('apps.backoffice.urls')),
+    path('dj-admin/', admin.site.urls),
+    re_path(r'.*$', include('apps.backoffice.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
